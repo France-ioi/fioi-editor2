616f1d8d246e0454c2b25ad0a5b1b868528382de
Sebastien Carlier <s.carlier@epixode.fr>
2015-10-01 14:20:12 +0000

    fix final recording result

--- src/services/recorder.js
+++ src/services/recorder.js
@@ -132,11 +132,12 @@ function RecorderFactory ($q, $interval, $sce, audio) {
             } else {
                afterCombineRecordings();
             }
-            function afterCombineRecordings (result) {
-               if (result) {
-                  var audioUrl = URL.createObjectURL(result.wav);
+            function afterCombineRecordings (combinedAudio) {
+               if (combinedAudio) {
+                  var audioUrl = URL.createObjectURL(combinedAudio.wav);
                   result.audioUrl = audioUrl;
                   result.safeAudioUrl = $sce.trustAsResourceUrl(audioUrl);
+                  result.audioEncoding = combinedAudio.encoding;
                }
                // Clear the recorder state.
                audio.clearRecordings();
